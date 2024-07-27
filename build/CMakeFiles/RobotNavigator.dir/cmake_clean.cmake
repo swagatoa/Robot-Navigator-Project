859ed2a760a315e03f8f4e09f49ac7baa8eca9e0
swagatoacharjee <swagato.acharjee@gmail.com>
2024-07-27 09:37:38 -0400

    VS code compatibility

---
+++ build/CMakeFiles/RobotNavigator.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RobotNavigator.dir/game.cpp.obj"
+  "CMakeFiles/RobotNavigator.dir/game.cpp.obj.d"
+  "CMakeFiles/RobotNavigator.dir/main.cpp.obj"
+  "CMakeFiles/RobotNavigator.dir/main.cpp.obj.d"
+  "CMakeFiles/RobotNavigator.dir/robot.cpp.obj"
+  "CMakeFiles/RobotNavigator.dir/robot.cpp.obj.d"
+  "RobotNavigator.exe"
+  "RobotNavigator.exe.manifest"
+  "RobotNavigator.pdb"
+  "libRobotNavigator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RobotNavigator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Work/c++/Robot-Navigator-Project
+# Build directory: C:/Work/c++/Robot-Navigator-Project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
